be81cc9fe67338db4450fe78a51f4dab3f148c58
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-05-21 08:50:54 +0000

    update linkedlist

--- basic-algorithm-operations/src/LinkedList.h
+++ basic-algorithm-operations/src/LinkedList.h
@@ -13,10 +13,16 @@ struct node
 
 node* createList(int data[], int len);
 void deleteList(node* ph);
-void printList(node* ph);
+void printList(node* ph, int limit = -1);
+node* get(node* ph, int index);
+int size(node* ph);
+
 int compareList(node* p1, node* p2, node** pdiff1 = NULL, node** pdiff2 = NULL, int* pos = NULL);
 int assertEquals(node* p1, node* p2);
 
 node* merge(node* p1, node* p2);
 node* mergeIter(node* p1, node* p2);
+
+node* reverse(node* ph);
+node* checkListLoop(node* ph);
 #endif
