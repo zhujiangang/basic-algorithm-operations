64c3dd394be3d7fa1b390de2f4ed79ac26ff98a6
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-05-25 03:05:58 +0000

    Fixed CombineMath.cpp error

--- basic-algorithm-operations/src/CombineMath.cpp
+++ basic-algorithm-operations/src/CombineMath.cpp
@@ -45,47 +45,6 @@ void combine(int in[], int len, int inBegin, int out[], int outBegin)
 	}
 }
 
-void Combin(int m,int n,int a) 
-{ 
-    if(m == n)
-	{ 
-        if(a>0)
-			printf("%d",a); 
-        while(n > 0) 
-			printf("%d",n--); 
-        printf("\n"); 
-        return; 
-    } 
-    else if(0 == n)
-	{ 
-        printf("%d ",a); 
-        return; 
-    } 
-    Combin(m-1,n-1,a*10+m); 
-    Combin(m-1,n,a); 
-}
-
-void search( int depth, int n, int m, int b[], int len)
-{
-	static int count = 0;
-	int i; 
-	if( m == 0 ) //满足条件则输出 
-	{ 
-		count++; 
-		printf("%d) ", count);
-		for( i = 0; i < len; i++ ) 
-			printf( "%d ", b[i] ); 
-		printf( "\n" ); 
-		
-		return ; 
-	} 
-	if( m > n || n <= 0 ) return; //依常识剪枝 
-	b[depth] = n; 
-	search( depth + 1, n - 1, m - 1 , b, len); 
-	b[depth] = 0; 
-	search( depth, n - 1, m, b, len ); 
-}
-
 void combine1(int input[], int n, int m, int output[], int outputLen, int depth)
 {
 	static int count = 0;
@@ -120,26 +79,27 @@ void combine1(int input[], int n, int m, int output[], int outputLen, int depth)
 void combine2(int input[], int n, int m, int output[], int outputLen)
 {
 	static int count = 0;
+
 	int i, j;
-
-	if(m <= 0)
-	{
-		count++;
-		cout<<count<<") ";
-		for(j = 0; j < outputLen; j++)
-		{
-			cout<<output[j]<<" ";
-		}
-		cout<<endl;
-		return;
-	}
-
 	for(i = n - 1; i >= m - 1; i--)
 	{
 		output[m - 1] = input[i];
+
+		//Greater than 1. If m == 1, we just need to select one, and this one has been done by "output[m - 1] = input[i]",
+		//the task was finished. We have got one result and should go to "else" to process this result.
 		if(m > 1)
 		{
-			combine2(input, n - 1, m - 1, output, outputLen);
+			combine2(input, i, m - 1, output, outputLen);
+		}
+		else
+		{
+			count++;
+			cout<<count<<") ";
+			for(j = 0; j < outputLen; j++)
+			{
+				cout<<output[j]<<" ";
+			}
+			cout<<endl;
 		}
 	}
 }
