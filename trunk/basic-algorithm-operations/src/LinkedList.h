ac9b2fc364762bf3cafafc83a90d6f7057203aaa
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-05-31 09:14:45 +0000

    add new operations for linkedlist

--- trunk/basic-algorithm-operations/src/LinkedList.h
+++ trunk/basic-algorithm-operations/src/LinkedList.h
@@ -1,7 +1,7 @@
 #ifndef _LINKEDLIST_H_
 #define _LINKEDLIST_H_
 
-struct node
+typedef struct node
 {
 	int data;
 	node *next;
@@ -9,33 +9,77 @@ struct node
 		:data(dataValue),next(nextValue)
 	{
 	}
-};
+}node;
 
+typedef struct cascade_node
+{
+	node* pnode;
+	cascade_node *next;
+	cascade_node(node* pnode_val = NULL, cascade_node* next_val = NULL)
+		:pnode(pnode_val),next(next_val)
+	{
+	}
+}cascade_node;
+
+/************************************************************************/
+/* 0. Basic Single linked list operations                               */
+/************************************************************************/
 node* createList(int data[], int len);
 void deleteList(node* ph);
 void printList(node* ph, int limit = -1);
 void showNode(node* ph);
 node* get(node* ph, int index);
 int size(node* ph);
-
 int compareList(node* p1, node* p2, node** pdiff1 = NULL, node** pdiff2 = NULL, int* pos = NULL);
 int assertEquals(node* p1, node* p2);
 
+/************************************************************************/
+/* 1. Reverse a single linkedlist                                       */
+/************************************************************************/
+node* reverse(node* ph);
+
+/************************************************************************/
+/* 2. find the last "index"th node, from 0                              */
+/************************************************************************/
+node* rget(node* ph, int index);
+
+/************************************************************************/
+/* 3. find the middle node(s) of the list "ph"                           
+if the node number of the list is odd, pMid1 is the mid node
+else if the node number is even, pMid1 is the first mid node, and pMid2 is the second mid node                                                                    */
+/************************************************************************/
+void getMiddleNode(node* ph, node** pMid1, node** pMid2 = NULL);
+
+/************************************************************************/
+/* 4. Merge two single linked list                                      */
+/************************************************************************/
 node* merge(node* p1, node* p2);
 node* mergeIter(node* p1, node* p2);
 
-node* reverse(node* ph);
+/************************************************************************/
+/* 5. Check if a single linked list has loop. If yes, find the loop entrance
+/************************************************************************/
 node* checkListLoop(node* ph);
 node* getListLoopEntrance(node* ph);
 
+/************************************************************************/
+/* 6. sort a single linked list                                         */
+/************************************************************************/
 node* qsort(node* ph);
 node* qsort1(node* ph);
 
-//find the last "index"th node, from 0
-node* rget(node* ph, int index);
+/************************************************************************/
+/* 7. Check if two linked list overlapped. If yes, find the first overlapped node
+/************************************************************************/
+node* getFirstOverlappedNode(node* p1, node* p2);
 
-//find the middle node(s) of the list "ph"
-//if the node number of the list is odd, pMid1 is the mid node
-//else if the node number is even, pMid1 is the first mid node, and pMid2 is the second mid node
-void getMiddleNode(node* ph, node** pMid1, node** pMid2 = NULL);
+/************************************************************************/
+/* 8. swap 2 linked list node, p1 and p2 are supposed to be one of nodes in the linked list
+/************************************************************************/
+node* swap(node* ph, node* p1, node* p2);
+
+/************************************************************************/
+/* 9. expand a cascade_node linked list to a simple linked list
+/************************************************************************/
+node* expand(cascade_node* pch);
 #endif
