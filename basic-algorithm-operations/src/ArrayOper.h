666247139342cb8b2376a1308cf9777e80de64ec
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-06-02 05:59:59 +0000

    add "ArrayOper.h"

---
+++ basic-algorithm-operations/src/ArrayOper.h
@@ -0,0 +1,15 @@
+#ifndef _ARRAY_OPER_H_
+#define _ARRAY_OPER_H_
+
+/************************************************************************/
+/* This file contains the algorithms based on arrays                    */
+/************************************************************************/
+
+
+/************************************************************************/
+/* 1. Print all the pairs in the array that their sum equals to input param "sum"
+      This array is in order.
+/************************************************************************/
+void printSumPairs(int a[], int n, int sum);
+void printSumPairsDup(int a[], int n, int sum);
+#endif
