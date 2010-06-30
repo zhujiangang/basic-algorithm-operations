9c2ecd0240c5c0fdbd4af8dbd0a21e91afcd3c20
cconger77 <cconger77@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-06-30 07:41:24 +0000

    add .bat script package

---
+++ bat-script/get-ip.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set TMP_FILE=ip.tmp
+
+ipconfig | findstr /C:"IP Address" >%TMP_FILE%
+
+set TMPSTR=
+
+FOR /F "tokens=2 delims=:" %%i in (%TMP_FILE%) do (
+  set TMPSTR=%%i
+)
+
+set IPSTR=
+FOR /F "tokens=1,2 delims= " %%i in ("%TMPSTR%") do (
+  set IPSTR=%%i
+)
+
+del /F /Q %TMP_FILE%
+
+echo %IPSTR%
---
+++ bat-script/set-classpath.bat
@@ -0,0 +1,15 @@
+
+setlocal ENABLEDELAYEDEXPANSION
+IF ERRORLEVEL 1 (
+    echo Unable to enable DELAYED EXPANSION
+    goto end
+)
+
+set JAR_LIBS=.
+for /f "delims=" %%i in ('dir /b /a-d /od /s .\lib\*.jar') do set JAR_LIBS=!JAR_LIBS!;%%i
+
+set classpath=%JAR_LIBS%;%classpath%
+
+echo %classpath%
+
+pause
