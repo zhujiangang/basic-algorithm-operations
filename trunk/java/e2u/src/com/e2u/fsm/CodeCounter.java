1c83dd935fda3860c740e7c39c2fe9481c4b67ac
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-09-10 09:24:35 +0000

    update fsm

---
+++ trunk/java/e2u/src/com/e2u/fsm/CodeCounter.java
@@ -0,0 +1,307 @@
+package com.e2u.fsm;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+public class CodeCounter
+{
+	private static final int STAT_NONE = 0;
+	private static final int STAT_FIRST_SLASH = 1;
+	private static final int STAT_SECOND_SLASH = 2;
+	private static final int STAT_STAR_AFTER_SLASH = 3;
+	private static final int STAT_STAR_IN_BLOCK_COMMENT = 4;
+	private static final int STAT_ESC_AFTER_SECOND_STAR = 5;
+	private static final int STAT_FIRST_QM = 6;
+	private static final int STAT_ESC_AFTER_FIRST_QM = 7;
+	private static final int STAT_CODE = 8;
+	private static final int STAT_BLOCK_COMMENT_END = 9;
+	
+	private int codeCount = 0;
+	private int commentCount = 0;
+	private int spaceCount = 0;
+	
+	private boolean isSpace(int ch)
+	{
+		if(ch == ' ' || ch == '\t')
+		{
+			return true;
+		}
+		return false;
+	}
+	private boolean isNewLine(int ch)
+	{
+		if(ch == '\n')
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public void parse(BufferedReader reader)
+	{
+		int fromStat = STAT_NONE;
+		int curStat = STAT_NONE;
+		int ch = -1;
+		try
+		{
+			while( (ch = reader.read()) != -1)
+			{
+				switch(curStat)
+				{
+					case STAT_NONE:
+					{
+						fromStat = STAT_NONE;
+						if(ch == '/')
+						{
+							curStat = STAT_FIRST_SLASH;
+						}
+						else if(ch == '"')
+						{
+							curStat = STAT_FIRST_QM;
+						}
+						else if(isSpace(ch))
+						{
+							
+						}
+						else if(ch == '\r')
+						{
+							
+						}
+						else if(isNewLine(ch))
+						{
+							spaceCount++;
+						}
+						//Any other keys
+						else
+						{
+							curStat = STAT_CODE;
+						}
+						break;
+					}
+					case STAT_FIRST_SLASH:
+					{
+						if(ch == '/')
+						{
+							curStat = STAT_SECOND_SLASH;
+						}
+						else if(ch == '*')
+						{
+							curStat = STAT_STAR_AFTER_SLASH;
+						}
+						else
+						{
+							curStat = STAT_CODE;
+							if(isNewLine(ch))
+							{								
+								codeCount++;
+							}
+						}
+						break;
+					}
+					case STAT_SECOND_SLASH:
+					{
+						if(isNewLine(ch))
+						{
+							curStat = STAT_NONE;
+							
+							commentCount++;
+							if(fromStat == STAT_CODE)
+							{
+								codeCount++;
+							}
+						}
+						//Any other keys
+						else
+						{
+							//stub
+						}
+						break;
+					}
+					case STAT_STAR_AFTER_SLASH:
+					{
+						if(ch == '*')
+						{
+							curStat = STAT_STAR_IN_BLOCK_COMMENT;
+						}
+						else if(isNewLine(ch))
+						{
+							commentCount++;
+						}
+						//Any other keys
+						else
+						{
+							//stub
+						}
+						break;
+					}
+					case STAT_STAR_IN_BLOCK_COMMENT:
+					{
+						if(ch == '\\')
+						{
+							curStat = STAT_ESC_AFTER_SECOND_STAR;
+						}
+						else if(ch == '/')
+						{
+							curStat = STAT_BLOCK_COMMENT_END;
+							commentCount++;
+						}
+						else if(ch == '\r')
+						{
+							
+						}
+						//Any other keys
+						else
+						{
+							if(isNewLine(ch))
+							{
+								commentCount++;
+							}
+							curStat = STAT_STAR_AFTER_SLASH;
+						}
+						break;
+					}
+					case STAT_ESC_AFTER_SECOND_STAR:
+					{						
+						if(isNewLine(ch))
+						{
+							commentCount++;
+						}
+						else if(ch == '\r')
+						{
+							
+						}
+						curStat = STAT_STAR_AFTER_SLASH;
+						break;
+					}
+					case STAT_FIRST_QM:
+					{
+						if(ch == '\\')
+						{
+							curStat = STAT_ESC_AFTER_FIRST_QM;
+						}
+						else if(ch == '"')
+						{
+							curStat = STAT_CODE;
+						}
+						//Any other keys
+						else
+						{
+							if(isNewLine(ch))
+							{
+								codeCount++;
+							}
+						}
+						break;
+					}
+					case STAT_ESC_AFTER_FIRST_QM:
+					{
+						if(isNewLine(ch))
+						{
+							codeCount++;
+						}
+						curStat = STAT_FIRST_QM;
+						break;
+					}
+					case STAT_CODE:
+					{
+						fromStat = STAT_CODE;
+						if(ch == '/')
+						{
+							curStat = STAT_FIRST_SLASH;
+						}
+						else if(ch == '"')
+						{
+							curStat = STAT_FIRST_QM;
+						}
+						else if(isSpace(ch))
+						{
+							
+						}
+						else if(isNewLine(ch))
+						{
+							curStat = STAT_NONE;
+							codeCount++;
+						}
+						else if(ch == '\r')
+						{
+							
+						}
+						//Any other keys
+						else
+						{
+							curStat = STAT_CODE;
+						}
+						break;
+					}
+					case STAT_BLOCK_COMMENT_END:
+					{
+						fromStat = STAT_BLOCK_COMMENT_END;
+						if(ch == '/')
+						{
+							curStat = STAT_FIRST_SLASH;
+						}
+						else if(ch == '"')
+						{
+							curStat = STAT_FIRST_QM;
+						}
+						else if(isSpace(ch))
+						{
+							
+						}
+						else if(ch == '\r')
+						{
+							
+						}
+						else if(isNewLine(ch))
+						{
+							curStat = STAT_NONE;
+						}
+						//Any other keys
+						else
+						{
+							curStat = STAT_CODE;
+						}
+						break;
+					}
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void error(String str)
+	{
+		System.out.println(str);
+	}
+	public void showResult()
+	{
+		System.out.printf("CODE COUNT = %d\n", codeCount);
+		System.out.printf("COMMENT COUNT = %d\n", commentCount);
+		System.out.printf("BLANK COUNT = %d\n", spaceCount);		
+	}
+	
+	public static void main(String[] args)
+	{
+		CodeCounter cc = new CodeCounter();
+		BufferedReader reader = null;
+		try
+		{
+			reader = new BufferedReader(new FileReader(new File("test.java")));
+			cc.parse(reader);
+			
+			cc.showResult();
+			
+			reader.close();
+		}
+		catch(Exception e)
+		{
+			// TODO: handle exception
+		}
+		
+	}
+}
---
+++ trunk/java/e2u/src/com/e2u/fsm/FSM.java
@@ -0,0 +1,218 @@
+package com.e2u.fsm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FSM
+{
+	private static final int STAT_NONE = 0;
+	private static final int STAT_ESC = 1;
+	private static final int STAT_VAR = 2;
+	private static final int STAT_ESC_VAR = 3;
+
+	public String parseGrammar(String str, Map<String, Object> out)
+	{
+		String result = "";
+		String varname = "";
+		char[] buffer = str.toCharArray();
+
+		int curStat = STAT_NONE;
+		for(int i = 0; i < buffer.length; i++)
+		{
+			switch(curStat)
+			{
+				case STAT_NONE:
+				{
+					if(buffer[i] == '\\')
+					{
+						curStat = STAT_ESC;
+					}
+					else if(buffer[i] == '%')
+					{
+						curStat = STAT_VAR;
+						varname = "";
+					}
+					else
+					{
+						result += buffer[i];
+					}
+					break;
+				}
+				case STAT_ESC:
+				{
+					result += buffer[i];
+					curStat = STAT_NONE;
+					break;
+				}
+				case STAT_VAR:
+				{
+					if(buffer[i] == '\\')
+					{
+						curStat = STAT_ESC_VAR;
+					}
+					else if(buffer[i] == '%')
+					{
+						curStat = STAT_NONE;
+						if(out.containsKey(varname))
+						{
+							result += out.get(varname);
+						}						
+					}
+					else
+					{
+						varname += buffer[i];
+					}
+					break;
+				}
+				case STAT_ESC_VAR:
+				{
+					varname += buffer[i];
+					curStat = STAT_VAR;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+	private OnAction appendResultAction = new AppendResult();
+	private OnAction appendVarNameAction = new AppendVarName();
+	private OnAction parVarNameAction = new ParseVarName();
+	private int[][] successor = 
+	{
+		{STAT_ESC, STAT_VAR, STAT_NONE},
+		{STAT_NONE},
+		{STAT_ESC_VAR, STAT_NONE, STAT_VAR},
+		{STAT_VAR}
+	};
+	private String[][] grammar = 
+	{
+		{"\\", "%", ""},
+		{""},
+		{"\\", "%", ""},
+		{""}
+	};
+	OnAction[][] actions = 
+	{
+		{null, null, appendResultAction},
+		{appendResultAction},
+		{null, parVarNameAction, appendVarNameAction},
+		{appendVarNameAction}
+	};
+	
+	
+	
+	public String parseGrammar1(String str, Map<String, Object> out)
+	{
+		char[] buffer = str.toCharArray();
+		StringBuffer result = new StringBuffer();
+		StringBuffer varname = new StringBuffer();
+		List params = new ArrayList(5);
+		params.add(result);
+		params.add(varname);
+		params.add(out);
+		params.add(null);
+		int curStat = STAT_NONE;
+		for(int i = 0; i < buffer.length; i++)
+		{			
+			params.set(3, new Integer(buffer[i]));
+			curStat = transition(curStat, buffer[i], params);
+		}
+		
+		return result.toString();
+	}
+	
+	private int transition(int state, char c, List params)
+	{
+		int result = -1;
+		String[] grammars = grammar[state];
+		for(int i = 0; i < grammars.length; i++)
+		{
+			if(grammars[i].isEmpty() || grammars[i].indexOf(c) != -1)
+			{
+				if(actions[state][i] != null)
+				{
+					actions[state][i].action(params);
+				}
+				result = successor[state][i];
+				break;    
+			}
+		}
+		return result;
+	}
+	                                       
+	private static class AppendResult implements OnAction
+	{
+		public void action(List params)
+		{
+			StringBuffer result = (StringBuffer)params.get(0);
+			char c = (char)((Integer)params.get(3)).intValue();
+			
+			result.append(c);
+		}
+	}
+	private static class AppendVarName implements OnAction
+	{
+		public void action(List params)
+		{
+			StringBuffer varname = (StringBuffer)params.get(1);
+			char c = (char)((Integer)params.get(3)).intValue();						
+			
+			varname.append(c);
+		}
+	}
+	private static class ParseVarName implements OnAction
+	{
+		public void action(List params)
+		{
+			StringBuffer result = (StringBuffer)params.get(0);
+			StringBuffer varname = (StringBuffer)params.get(1);
+			Map<String, Object> map = (Map<String, Object>)params.get(2);
+			
+			if(map.containsKey(varname.toString()))
+			{
+				result.append(map.get(varname.toString()));
+			}
+			
+			varname.setLength(0);
+		}
+	}
+	
+	private static interface OnAction
+	{
+		public void action(List params);
+	}
+	
+	private static void test()
+	{
+		FSM fsm = new FSM();
+		Map<String, Object> out = new HashMap<String, Object>();
+		
+		out.put("p1", "xyz");
+		out.put("p2", "8888");
+		out.put("Va%lue Onl\\%y@sME", "zaq");
+		
+		String input = "\\\\32 -- %p1% -- %p2% -- %Va\\%lue Onl\\\\\\%y@sME% -- qw1";
+		String strings[] = 
+		{
+			"\\\\32 -- %p1% -- %p2% -- %Va\\%lue Onl\\\\\\%y@sME% -- qw1",
+			"\\\\32%p1%%p2%%Va\\%lue Onl\\\\\\%y@sME%qw1"
+		};
+		String result = null;
+
+		for(int i = 0; i < strings.length; i++)
+		{
+			result = fsm.parseGrammar(strings[i], out);
+			System.out.println(result);
+			
+			result = fsm.parseGrammar1(strings[i], out);
+			System.out.println(result);
+		}
+	}
+	public static void main(String[] args)
+	{
+		test();
+	}
+}
