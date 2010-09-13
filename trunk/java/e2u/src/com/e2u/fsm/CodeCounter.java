205ec5a9751279fe27cd80bfe627f5273631ab39
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-09-13 08:49:07 +0000

    update FSM/CoderCounter.java

--- trunk/java/e2u/src/com/e2u/fsm/CodeCounter.java
+++ trunk/java/e2u/src/com/e2u/fsm/CodeCounter.java
@@ -2,7 +2,6 @@ package com.e2u.fsm;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 
 public class CodeCounter
@@ -17,6 +16,8 @@ public class CodeCounter
 	private static final int STAT_ESC_AFTER_FIRST_QM = 7;
 	private static final int STAT_CODE = 8;
 	private static final int STAT_BLOCK_COMMENT_END = 9;
+	private static final int STAT_FIRST_SQM = 10;
+	private static final int STAT_ESC_AFTER_FIRST_SQM = 11;
 	
 	private int codeCount = 0;
 	private int commentCount = 0;
@@ -274,6 +275,204 @@ public class CodeCounter
 		}
 	}
 	
+	public void parse1(BufferedReader reader)
+	{
+		String line = null;
+		int curStat = STAT_NONE;
+		int ch = -1;
+		try
+		{
+			while( (line = reader.readLine()) != null)
+			{
+				line = line.trim();
+				if(line.isEmpty())
+				{
+//					if(curStat == STAT_STAR_AFTER_SLASH)
+//					{
+//						commentCount++;
+//					}
+//					else
+//					{
+//						spaceCount++;
+//					}
+					spaceCount++;
+					continue;
+				}
+
+				if(curStat == STAT_STAR_AFTER_SLASH || curStat == STAT_STAR_IN_BLOCK_COMMENT || curStat == STAT_ESC_AFTER_SECOND_STAR)
+				{
+					
+				}
+				else
+				{
+					curStat = STAT_NONE;
+				}
+				for(int i = 0, size = line.length(); i < size; i++)
+				{
+					ch = line.charAt(i);				
+					switch(curStat)
+					{
+						case STAT_NONE:
+						{
+							if(ch == '/')
+							{
+								curStat = STAT_FIRST_SLASH;
+							}
+							else if(ch == '"')
+							{
+								curStat = STAT_FIRST_QM;
+							}
+							else if(ch == '\'')
+							{
+								curStat = STAT_FIRST_SQM;
+							}
+							if(i == size - 1)
+							{
+								codeCount++;
+							}
+							break;
+						}
+						case STAT_FIRST_SLASH:
+						{
+							if(ch == '/')
+							{
+								curStat = STAT_SECOND_SLASH;
+							}
+							else if(ch == '*')
+							{
+								curStat = STAT_STAR_AFTER_SLASH;
+							}
+							else
+							{
+								curStat = STAT_NONE;
+							}
+							if(i == size - 1)
+							{
+								if(curStat == STAT_SECOND_SLASH || curStat == STAT_STAR_AFTER_SLASH)
+								{
+									commentCount++;
+								}
+								else
+								{
+									codeCount++;
+								}								
+							}
+							break;
+						}
+						case STAT_SECOND_SLASH:
+						{
+							if(i == size - 1)
+							{
+								commentCount++;
+							}
+							break;
+						}
+						case STAT_STAR_AFTER_SLASH:
+						{
+							if(ch == '*')
+							{
+								curStat = STAT_STAR_IN_BLOCK_COMMENT;
+							}
+							if(i == size - 1)
+							{
+								commentCount++;
+							}
+							break;
+						}
+						case STAT_STAR_IN_BLOCK_COMMENT:
+						{
+							if(ch == '\\')
+							{
+								curStat = STAT_ESC_AFTER_SECOND_STAR;
+							}
+							else if(ch == '/')
+							{
+								curStat = STAT_NONE;
+								if(i != size - 1)
+								{
+									commentCount++;
+								}
+							}
+							//Any other keys
+							else
+							{
+								curStat = STAT_STAR_AFTER_SLASH;
+							}
+							if(i == size - 1)
+							{
+								commentCount++;
+							}
+							break;
+						}
+						case STAT_ESC_AFTER_SECOND_STAR:
+						{						
+							curStat = STAT_STAR_AFTER_SLASH;
+							if(i == size - 1)
+							{
+								commentCount++;
+							}
+							break;
+						}
+						case STAT_FIRST_QM:
+						{
+							if(ch == '\\')
+							{
+								curStat = STAT_ESC_AFTER_FIRST_QM;
+							}
+							else if(ch == '"')
+							{
+								curStat = STAT_NONE;
+							}
+							if(i == size - 1)
+							{
+								codeCount++;
+							}
+							break;
+						}
+						case STAT_ESC_AFTER_FIRST_QM:
+						{
+							curStat = STAT_FIRST_QM;
+							if(i == size - 1)
+							{
+								codeCount++;
+							}
+							break;
+						}
+						case STAT_FIRST_SQM:
+						{
+							if(ch == '\\')
+							{
+								curStat = STAT_ESC_AFTER_FIRST_SQM;
+							}
+							else if(ch == '\'')
+							{
+								curStat = STAT_NONE;
+							}
+							if(i == size - 1)
+							{
+								codeCount++;
+							}
+							break;
+						}
+						case STAT_ESC_AFTER_FIRST_SQM:
+						{
+							curStat = STAT_FIRST_SQM;
+							if(i == size - 1)
+							{
+								codeCount++;
+							}
+							break;
+						}
+					}
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 	private static void error(String str)
 	{
 		System.out.println(str);
@@ -282,7 +481,8 @@ public class CodeCounter
 	{
 		System.out.printf("CODE COUNT = %d\n", codeCount);
 		System.out.printf("COMMENT COUNT = %d\n", commentCount);
-		System.out.printf("BLANK COUNT = %d\n", spaceCount);		
+		System.out.printf("BLANK COUNT = %d\n", spaceCount);
+
 	}
 	
 	public static void main(String[] args)
@@ -292,7 +492,7 @@ public class CodeCounter
 		try
 		{
 			reader = new BufferedReader(new FileReader(new File("test.java")));
-			cc.parse(reader);
+			cc.parse1(reader);
 			
 			cc.showResult();
 			
