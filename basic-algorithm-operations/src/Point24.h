ad832308882ab4136943cd78532edfdae9025ab6
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2010-09-21 07:55:53 +0000

    Add Point24 algorithm to ToolSet

---
+++ basic-algorithm-operations/src/Point24.h
@@ -0,0 +1,11 @@
+#ifndef _POINT_24_H_
+#define _POINT_24_H_
+
+extern const double PRECISION; 
+extern const int COUNT_OF_NUMBER; 
+extern const int NUMBER_TO_CAL; 
+
+bool Search(int n);
+void testPoint24();
+
+#endif
