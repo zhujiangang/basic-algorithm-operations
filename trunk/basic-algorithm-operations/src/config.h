1f56291047040e07a1e0cf4949ff51985ac1a663
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2011-02-17 01:52:59 +0000

    Add bao/config.h

---
+++ trunk/basic-algorithm-operations/src/config.h
@@ -0,0 +1,16 @@
+#ifndef _BAO_CONFIG_H_
+#define _BAO_CONFIG_H_
+
+#define ARRAY_OPER_TEST
+#define ARRAY_SORT_TEST
+#define BIN_TREE_TEST
+#define BIT_OPER_TEST
+#define COMBINE_MATH_TEST
+//#define FILE_OPER_TEST
+#define LINKED_LIST_TEST
+#define LINKED_LIST_EX_TEST
+//#define POINT24_TEST
+#define STACK_QUEUE_TEST
+#define STRING_OPER_TEST
+
+#endif
