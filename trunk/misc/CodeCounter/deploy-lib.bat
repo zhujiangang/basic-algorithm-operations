40e368cd661a578b09330ff052b7280062cc6844
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2011-05-27 07:47:29 +0000

    Update deploy-lib.bat

--- trunk/misc/CodeCounter/deploy-lib.bat
+++ trunk/misc/CodeCounter/deploy-lib.bat
@@ -1,10 +1,15 @@
 @echo off
 
-set DST_DIR=..\..\..\cosps\ListCtrl\ListCtrlDemo\Lib
-set SRC_DIR=.\Debug
+set DST_DIR=..\..\..\cosps\ListCtrl\ListCtrlDemo
 
-if exist "%SRC_DIR%\CodeCounter.lib" (
-	copy /Y /B "%SRC_DIR%\CodeCounter.lib" "%DST_DIR%\SCC.lib"
+if exist ".\Debug\CodeCounter.lib" (
+	copy /Y /B ".\Debug\CodeCounter.lib" "%DST_DIR%\Lib\SCCd.lib"
 )
+if exist ".\Release\CodeCounter.lib" (
+	copy /Y /B ".\Release\CodeCounter.lib" "%DST_DIR%\Lib\SCC.lib"
+)
+
+copy /Y /B ".\FileParser.h" "%DST_DIR%\Include\FileParser.h"
+copy /Y /B ".\CFileParser.h" "%DST_DIR%\Include\CFileParser.h"
 
 pause
