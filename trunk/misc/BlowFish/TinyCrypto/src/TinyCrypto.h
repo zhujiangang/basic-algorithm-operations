bf19286eb004fe8e630259645d531aaac0130658
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2011-07-26 08:14:38 +0000

    Update Parameters to const type

--- trunk/misc/BlowFish/TinyCrypto/src/TinyCrypto.h
+++ trunk/misc/BlowFish/TinyCrypto/src/TinyCrypto.h
@@ -3,8 +3,15 @@
 
 #pragma once
 
+#define ERROR_CODE_CALC_MD5_FAILED			-1
+#define ERROR_CODE_BASE32_ENCODE_FAILED		-2
+#define ERROR_CODE_BASE32_MAP_FAILED		-3
+#define ERROR_CODE_BASE32_DECODE_FAILED		-4
+#define ERROR_CODE_BASE32_UNMAP_FAILED		-5
+#define ERROR_CODE_VALIDATION_FAILED		-6
+
 int CalcMD5(const unsigned char* in, int nInLen, unsigned char* out);
-int Encrypt(const unsigned char* in, int nInLen, unsigned char* out, int nOutLen, unsigned char* key, int nKeyLen);
-int Validate(const unsigned char* in, int nInLen, const unsigned char* pBase32, int nBase32Len, unsigned char* key, int nKeyLen);
+int Encrypt(const unsigned char* in, int nInLen, unsigned char* out, int nOutLen, const unsigned char* key, int nKeyLen);
+int Validate(const unsigned char* in, int nInLen, const unsigned char* pBase32, int nBase32Len, const unsigned char* key, int nKeyLen);
 
 #endif
