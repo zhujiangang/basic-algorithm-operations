f4b831002f989efb63e2025d7e2a0a34a9d4e151
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2012-02-19 09:48:44 +0000

    refactor logging

--- trunk/java/lc/src/main/java/com/bao/lc/App.java
+++ trunk/java/lc/src/main/java/com/bao/lc/App.java
@@ -1,5 +1,9 @@
 package com.bao.lc;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.PropertyConfigurator;
@@ -14,16 +18,26 @@ public class App
 {
 	static
 	{		
-		PropertyConfigurator.configure(App.class.getResource("/commons-logging.properties"));
+		PropertyConfigurator.configure(App.class.getResource("/log4j.properties"));
 	}
 
 	private static Log log = LogFactory.getLog(App.class);
 
 	public static void main(String[] args)
 	{
-		log.info("Hello World!");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z");
+		log.info("App started at: " + dateFormat.format(new Date()));
 		
-		CnetUser cnetUser = new CnetUser();
-		cnetUser.login("test1", "123");
+		try
+		{
+			CnetUser cnetUser = new CnetUser();
+			cnetUser.login("test1", "123");
+		}
+		catch(Exception e)
+		{
+			log.error(e.toString(), e);
+		}
+
+		log.info("App stopped at: " + dateFormat.format(new Date()));
 	}
 }
