3b0b61cb6550dd96b9a003cc4ea5aaac57e9bf14
cconger77@gmail.com <cconger77@gmail.com@9640ac98-0c92-11df-8f49-cf7c468afe83>
2012-06-08 07:51:17 +0000

    update bootstrap.bat

--- trunk/java/lc/bootstrap.bat
+++ trunk/java/lc/bootstrap.bat
@@ -1,5 +1,13 @@
-@ECHO off
+@echo off
 
+@rem enable delay expansion
+setlocal ENABLEDELAYEDEXPANSION
+if ERRORLEVEL 1 (
+    echo Unable to enable DELAYED EXPANSION
+    goto end
+)
+
+@rem remove the trailing \
 set ARGV0=%~dp0
 :TRIM_LAST_SLASH_A
 if not "%ARGV0:~-1%"=="\" (
@@ -11,14 +19,14 @@ if not "%ARGV0:~-1%"=="\" (
 :TRIM_LAST_SLASH_Z
 SET THE_BOOTSTRAP_DIR=%ARGV0%
 
+@rem set work dirs
 SET CUR_DIR=%CD%
 SET WORK_DIR=%THE_BOOTSTRAP_DIR%
-
 CD /d "%WORK_DIR%"
 
-SET LOCALCLASSPATH=%WORK_DIR%\conf\usr;%WORK_DIR%\conf\default
-for %%i in (%WORK_DIR%\boot\*.jar) do call %WORK_DIR%\bin\lcp.bat %%~fi
-for %%i in (%WORK_DIR%\lib\*.jar) do call %WORK_DIR%\bin\lcp.bat %%~fi
+set LOCALCLASSPATH=%WORK_DIR%\conf\usr;%WORK_DIR%\conf\default
+for /f "delims=" %%i in ('dir /b /a-d /od /s "%WORK_DIR%\boot\*.jar"') do set LOCALCLASSPATH=!LOCALCLASSPATH!;%%i
+for /f "delims=" %%i in ('dir /b /a-d /od /s "%WORK_DIR%\lib\*.jar"') do set LOCALCLASSPATH=!LOCALCLASSPATH!;%%i
 echo %LOCALCLASSPATH%
 
 if not "%*"=="" (
@@ -27,7 +35,7 @@ if not "%*"=="" (
   set JAVA_PARAMETERS=com.bao.lc.App
 )
 
-java -cp %LOCALCLASSPATH% %JAVA_PARAMETERS%
+java -cp "%LOCALCLASSPATH%" %JAVA_PARAMETERS%
 
 :END
 CD /d "%CUR_DIR%"
