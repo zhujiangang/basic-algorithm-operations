d4c394dd37d14f60ad76e564d1881071dcf4abda
chaosllgao <chaosllgao@9640ac98-0c92-11df-8f49-cf7c468afe83>
2013-02-24 13:21:47 +0000

    Update CombineMath

--- trunk/basic-algorithm-operations/src/CombineMath.h
+++ trunk/basic-algorithm-operations/src/CombineMath.h
@@ -5,29 +5,29 @@
 /************************************************************************/
 /* P(n,n)                                                               */
 /************************************************************************/
-void perm(int in[], int begin, int end);
+void perm_Pnn(int in[], int begin, int end);
 
 
 /************************************************************************/
 /* P(n,m)                                                               */
 /************************************************************************/
-void perm(int in[], int n, int m, int out[], int depth, int used[]);
+void perm_Pnm(int in[], int n, int m, int out[], int depth, int used[]);
 
 
 /************************************************************************/
 /* collection. 2^n                                                      */
 /************************************************************************/
-void combine(int in[], int len, int inBegin, int out[], int outBegin);
+void combine_2n(int in[], int len, int inBegin, int out[], int outBegin);
 
 /************************************************************************/
 /* C(n, m) = C(n-1, m-1) + C(n-1, m)                                   */
 /************************************************************************/
-void combine1(int in[], int n, int m, int out[], int outLen, int depth);
+void combine_Cnm(int in[], int n, int m, int out[], int outLen, int depth);
 
 /*************************************************************************/
 /* C(n, m) = C(n-1, m-1) + C(n-2, m-1) + C(n-3, m-1) + .... + C(m-1, m-1)*/
 /*************************************************************************/
-void combine2(int in[], int n, int m, int out[], int outLen);
+void combine_Cnm2(int in[], int n, int m, int out[], int outLen);
 
 
 void testCombineMath();
